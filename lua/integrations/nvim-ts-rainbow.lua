a6d386236f7218f2b84ee7b2e8000ac51179ee30
helywin <jiang770882022@hotmail.com>
2022-08-08 10:17:29 +0800

    add nvim-ts-rainbow support

---
+++ lua/integrations/nvim-ts-rainbow.lua
@@ -0,0 +1,11 @@
+local theme = require("base46").get_colors "base_16"
+
+return {
+    rainbowcol1 = { fg = theme.red },
+    rainbowcol2 = { fg = theme.yellow },
+    rainbowcol3 = { fg = theme.orange },
+    rainbowcol4 = { fg = theme.green },
+    rainbowcol5 = { fg = theme.blue },
+    rainbowcol6 = { fg = theme.cyan },
+    rainbowcol7 = { fg = theme.purple },
+}
